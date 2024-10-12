1b96ca2346e5aedb28e1f5af83f2bf4ec6456609
ia74 <68617740+ia74@users.noreply.github.com>
2024-10-12 00:53:01 -0500

    - Add twitch source

---
+++ Twitch.src/Twitch.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const Plugin = require(path.resolve("./src/classes/Plugin"));
+
+class Twitch extends Plugin {
+	constructor() {
+		super("Twitch", "Freedeck", "Twitch", false);
+		this.version = "2.0.0";
+	}
+	onInitialize() {
+		console.log("Initialized Twitch plugin");
+		this.setJSServerHook("twitch/Hook.js");
+		this.setJSClientHook("twitch/Hook.js");
+		this.registerNewType("Viewer Count", "t.vc", {streamer:""}, "text");
+		return true;
+	}
+}
+
+module.exports = {
+	exec: () => new Twitch(),
+	class: Twitch,
+};
---
+++ Twitch.src/twitch/Hook.js
@@ -0,0 +1,28 @@
+function getViewerCount(user) {
+  return new Promise((resolve, reject) => {
+    fetch("https://content-dl.freedeck.app/TwitchPlugin.php?streamer=" + user, {
+      method: "GET",
+    }).then((res) => {
+      res.text().then((data) => {
+        resolve(data);
+      });
+    })
+  })
+}
+
+function updateViewerCount() {
+  document.querySelectorAll(".button").forEach((button) => {
+    if(!button.getAttribute("data-interaction")) return;
+    const inter = JSON.parse(button.getAttribute("data-interaction"));
+    if(inter.type !== "t.vc") return;
+    getViewerCount(inter.data.streamer).then((vc) => {
+      button.innerText = inter.data.streamer+": " +vc;
+    });
+  });
+}
+
+universal.listenFor("page_change", updateViewerCount);
+updateViewerCount();
+setInterval(() => {
+  updateViewerCount();
+}, 60000);
